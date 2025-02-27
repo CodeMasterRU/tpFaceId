e0c228163deaf4db1fcbf37196a1167a1002a4b0
CodeMasterRU <vitaly.skvortsov@ynov.com>
2025-02-27 16:33:35 +0100

    premier commit

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ unknown_image = face_recognition.load_image_file('face2.jpg')
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 results = face_recognition.compare_faces([biden_encoding],unknown_encoding)
+print("lol")
 print(str(results))
